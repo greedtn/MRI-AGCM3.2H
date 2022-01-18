ca39c2532390f65a6d67ceed186431cf54c405e8
naoto <naoto.tsuda63@gmail.com>
2022-01-19 00:09:59 +0900

    POT_ALLでのミスを修正

--- export_pot/func.py
+++ export_pot/func.py
@@ -13,13 +13,13 @@ def calc_pot_all(thr, dir_path, param_name, model_name):
     POT = [[0] for _ in range(79 * 79)]  # 閾値を超えるデータ(POT[-1]を使用するために初期値を設定)を格納する2d-array
     POT_IDX = [[-168] for _ in range(79 * 79)]  # 閾値を超えるデータのindex(POT_IDX[-1]を使用するために初期値を設定)を格納する2d-array
     CNT = 0  # indexカウント用の変数
-    NOW = 0
+    NOW = 0  # 進捗管理用の変数
 
     DIR_PATH = dir_path
     DIR = os.listdir(DIR_PATH)
 
     for filename in DIR:
-        if filename[:3] != "Jpn":
+        if filename[:3] != "Jpn":  # 日本のデータのみ使用
             continue
         grbs = pygrib.open(DIR_PATH + filename)
         grbs = grbs.select(parameterName=param_name)
@@ -31,8 +31,8 @@ def calc_pot_all(thr, dir_path, param_name, model_name):
                 m = pot[0][i]
                 n = pot[1][i]
                 d = data[m][n]
-                # decluster
-                if CNT > POT_IDX[79 * m + n][-1] + 168:
+                # decluster(1週間以上間隔を空ける)
+                if CNT > POT_IDX[79 * m + n][-1] + 24 * 7:
                     POT[79 * m + n].append(d)
                     POT_IDX[79 * m + n].append(CNT)
                 else:
@@ -42,9 +42,8 @@ def calc_pot_all(thr, dir_path, param_name, model_name):
 
         NOW += 1
         print(f'----- {NOW} / 300 done')
-        if NOW == 300:
-            break
 
+    # 初期値を削除
     for i in range(79 * 79):
         POT[i].pop(0)
         POT_IDX[i].pop(0)
@@ -56,8 +55,5 @@ def calc_pot_all(thr, dir_path, param_name, model_name):
     with open('../pot_csv/' + model_name + '_POT_INDEX.csv', 'w') as file:
         writer = csv.writer(file, lineterminator='\n')
         writer.writerows(POT_IDX)
-    with open('../pot_csv/' + model_name + '_POT_CNT.csv', 'w') as file:
-        writer = csv.writer(file, lineterminator='\n')
-        writer.writerow([CNT])
-
+    print(CNT)
     return
